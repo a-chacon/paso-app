657d56f31539eeee7f387d1d41f6fb21e94b416b
a-chacon <andres.ch@protonmail.com>
2022-07-05 17:57:46 -0400

    refactor: update dependencies

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,9 @@
 require "minitest/cc"
-Minitest::Cc.start_coverage
+Minitest::Cc.start
 
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
-
 
 class ActiveSupport::TestCase
   # Run tests in parallel with specified workers
